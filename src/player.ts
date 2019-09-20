5e1112d8e2c82331df96860862bc9a7f4e442f1c
mdsboy <modasokun@gmail.com>
2019-09-20 19:01:39 +0900

    Playerのupdateを関数に分けて整理

--- src/player.ts
+++ src/player.ts
@@ -12,28 +12,68 @@ export default class Player {
   private speed: number
   private circle: Circle
   private angle: number
-  private rotate: boolean
+  private isRotating: boolean
   private center: Point
-  private rotate_circle: Circle | null
+  private rotateCircle: Circle | null
   private len: number
   private ay: number
 
   constructor() {
     this.speed = 5
     this.circle = new Circle(new Point(100, 100), 30)
-    this.rotate_circle = null
+    this.rotateCircle = null
     this.ay = 0
   }
 
   public draw() {
     dm.circle(this.circle, '#000', true)
 
-    if (this.rotate_circle) {
-      dm.circle(this.rotate_circle, '#000', false)
+    if (this.rotateCircle) {
+      dm.circle(this.rotateCircle, '#000', false)
     }
   }
 
   public update(): void {
+    if (InputMouse.isMouseLeftDown()) {
+      if (!this.isRotating) {
+        this.rotateStart()
+      }
+      this.rotate()
+    } else {
+      this.rotateEnd()
+      this.notRotate()
+    }
+  }
+
+  private rotateStart(): void {
+    this.isRotating = true
+    this.center = InputMouse.getMousepos()
+
+    this.len = this.center.dist(this.circle.pos)
+    this.rotateCircle = new Circle(this.center, this.len)
+
+    const x = this.circle.pos.x - this.center.x
+    const y = this.circle.pos.y - this.center.y
+    this.angle = radianToDegree(Math.atan2(y, x))
+    console.log(this.angle)
+
+    this.ay = 0
+  }
+
+  private rotate(): void {
+    this.angle += 1
+    const x = this.center.x + this.len * Math.cos(degreeToRadian(this.angle))
+    const y = this.center.y + this.len * Math.sin(degreeToRadian(this.angle))
+    this.circle.pos = new Point(x, y)
+  }
+
+  private rotateEnd(): void {
+    this.rotateCircle = null
+    this.isRotating = false
+    this.angle = 0
+  }
+
+  private notRotate(): void {
     let pos = this.circle.pos
 
     if (InputKey.isKeyDown(KeyCode.A)) {
@@ -43,37 +83,8 @@ export default class Player {
       pos.x += this.speed
     }
 
-    if (InputMouse.isMouseLeftDown()) {
-      if (!this.rotate) {
-        this.rotate = true
-        this.center = InputMouse.getMousepos()
-
-        this.len = this.center.dist(pos)
-        this.rotate_circle = new Circle(this.center, this.len)
-
-        const x = pos.x - this.center.x
-        const y = pos.y - this.center.y
-        this.angle = radianToDegree(Math.atan2(y, x))
-        console.log(this.angle)
-
-        this.ay = 0
-      }
-    } else {
-      this.rotate = false
-      this.angle = 0
-    }
-
-    if (this.rotate) {
-      this.angle += 1
-      pos.x =
-        this.center.x + this.len * Math.cos(degreeToRadian(this.angle))
-      pos.y =
-        this.center.y + this.len * Math.sin(degreeToRadian(this.angle))
-    } else {
-      pos.y += this.ay
-      this.ay += 1
-    }
-
+    pos.y += this.ay
+    this.ay += 1
     if (pos.y >= dm.height) {
       pos.y = dm.height
       this.ay = 0
