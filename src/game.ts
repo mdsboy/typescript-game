357bd74be89fd3e0ab200aa22ae13e09ce32c131
mdsboy <modasokun@gmail.com>
2019-09-20 17:59:08 +0900

    クリックした点を中心に回転

--- src/game.ts
+++ src/game.ts
@@ -6,18 +6,27 @@ import dm from './lib/drawManager'
 import SceneBase from './lib/sceneBase'
 import Rect from './lib/rect'
 import Circle from './lib/circle'
+import { InputMouse } from './lib/inputMouse'
+import {radianToDegree, degreeToRadian} from './lib/util'
 
 export default class Game implements SceneBase {
   private pos: Point
   private speed: number
   private rect: Rect
   private circle: Circle
+  private angle: number
+  private rotate: boolean
+  private center: Point
+  private rotate_circle: Circle | null
+  private len: number
+  private ay: number
 
   constructor() {
     this.pos = new Point(100, 100)
     this.speed = 5
     this.rect = new Rect(Point.zero, 200, 200)
     this.circle = new Circle(this.pos, 30)
+    this.rotate_circle = null
   }
 
   public draw() {
@@ -25,6 +34,10 @@ export default class Game implements SceneBase {
     dm.circle(this.circle, '#000', true)
     dm.line(new Point(50, 100), new Point(200, 300), 3, '#000')
     dm.string(new Point(300, 300), 'abcdef', 300, '000')
+
+    if (this.rotate_circle) {
+      dm.circle(this.rotate_circle, '#000', false)
+    }
   }
 
   public update(): SceneBase {
@@ -41,7 +54,31 @@ export default class Game implements SceneBase {
       this.pos.y += this.speed
     }
 
-    this.circle.pos = this.pos
+    if (InputMouse.isMouseLeftDown()) {
+      if (!this.rotate) {
+        this.rotate = true
+        this.center = InputMouse.getMousepos()
+
+        this.len = this.center.dist(this.circle.pos)
+        this.rotate_circle = new Circle(this.center, this.len)
+
+        const x = this.circle.pos.x - this.center.x
+        const y = this.circle.pos.y - this.center.y
+        this.angle = radianToDegree(Math.atan2(y, x))
+        console.log(this.angle)
+      }
+    } else {
+      this.rotate = false
+      this.angle = 0
+    }
+
+    if (this.rotate) {
+      this.angle += 2
+      this.circle.pos.x =
+        this.center.x + this.len * Math.cos(degreeToRadian(this.angle))
+      this.circle.pos.y =
+        this.center.y + this.len * Math.sin(degreeToRadian(this.angle))
+    }
 
     return this
   }
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,11 @@
 export function between(x: number, y: number, z: number): boolean {
-    return x <= y && y <= z
+  return x <= y && y <= z
+}
+
+export function radianToDegree(radian: number): number {
+  return (radian * 180) / Math.PI
+}
+
+export function degreeToRadian(degree: number): number {
+  return (degree * Math.PI) / 180
 }
